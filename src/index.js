e8bef6ee07a74650d700e2d5a111d6baab4eb141
Kalin Hristov <kalin0169@gmail.com>
2023-01-22 14:05:24 +0200

    Render home page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
 app.use('/static', express.static('public'));
 
+app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  //   res.send('Hello World');
+  res.render('index');
 });
 
 app.listen(3000, () => console.log(`App is listening on port 3000`));
