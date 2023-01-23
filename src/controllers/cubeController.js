673dde3f83c129b8c80085cbcdb6066d8c651410
Kalin Hristov <kalin0169@gmail.com>
2023-01-23 21:51:14 +0200

    Render static details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,32 +1,43 @@
 const router = require('express').Router();
-const cubes = require('../db.json');
-const fs = require('fs/promises');
-
-const path = require('path');
+// const cubes = require('../db.json');
+// const fs = require('fs/promises');
+// const path = require('path');
+const cubeService = require('../services/cubeService');
 
 router.get('/create', (req, res) => {
   res.render('create');
 });
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
   const cube = req.body;
-  //Validate
+
   if (cube.name.length < 2) {
     return res.status(400).send('Invalid request');
   }
-  //Save data
-  // console.log(path.resolve('src', 'db.json'));
-  cubes.push(cube);
-  fs.writeFile(path.resolve('src', 'db.json'), JSON.stringify(cubes, '', 4), {
-    encoding: 'utf-8',
-  })
-    .then(() => {
-      //Redirect to page
-      res.redirect('/');
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
+
+  try {
+    await cubeService.save(cube);
+    res.redirect('/');
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
+
+router.get('/details/:id', (req, res) => {
+  res.render('details');
+});
+
+//   console.log(path.resolve('src', 'db.json'));
+//   cubes.push(cube);
+//   fs.writeFile(path.resolve('src', 'db.json'), JSON.stringify(cubes, '', 4), {
+//     encoding: 'utf-8',
+//   })
+//     .then(() => {
+//       res.redirect('/');
+//     })
+//     .catch((err) => {
+//       res.status(400).send(err);
+//     });
+// });
 
 module.exports = router;
---
+++ src/services/cubeService.js
@@ -0,0 +1,13 @@
+const fs = require('fs/promises');
+const cubes = require('../db.json');
+const path = require('path');
+
+exports.save = (cube) => {
+  cubes.push(cube);
+
+  let textData = JSON.stringify(cubes, '', 4);
+
+  return fs.writeFile(path.resolve('src', 'db.json'), textData, {
+    encoding: 'utf-8',
+  });
+};
