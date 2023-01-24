df752b6fbc3830413357a4413c5cb591ec27a1db
Kalin Hristov <kalin0169@gmail.com>
2023-01-24 18:12:26 +0200

    Add search

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -3,6 +3,17 @@ const path = require('path');
 
 const cubes = require('../db.json');
 
+exports.getAll = (search = '', fromInput, toInput) => {
+  const from = Number(fromInput) || 0;
+  const to = Number(toInput) || 6;
+
+  const result = cubes
+    .filter((x) => x.name.toLowerCase().includes(search.toLowerCase()))
+    .filter((x) => x.difficultyLevel >= from && x.difficultyLevel <= to);
+
+  return result;
+};
+
 exports.getOne = (cubeId) => cubes[cubeId];
 
 exports.save = (cube) => {
